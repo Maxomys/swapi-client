cf607249ef3dcc019fec240a0067fe809cef3df9
Maxomys <mistrz7losos@live.com>
2024-12-07 16:56:05 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PeopleModule } from './people/people.module';
+import { FilmsModule } from './films/films.module';
+import { StarshipsModule } from './starships/starships.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { SpeciesModule } from './species/species.module';
+import { PlanetsModule } from './planets/planets.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+      playground: false,
+    }),
+    PeopleModule,
+    FilmsModule,
+    StarshipsModule,
+    VehiclesModule,
+    SpeciesModule,
+    PlanetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
