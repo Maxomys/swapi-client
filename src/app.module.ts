4925895dd888a3d7a88b7f466f1544c3ef7669b8
Maxomys <mistrz7losos@live.com>
2024-12-11 03:36:00 +0100

    remove appservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -37,6 +36,5 @@ import { ConfigModule } from '@nestjs/config';
     SwapiModule,
     AnalysisModule,
   ],
-  providers: [AppService],
 })
 export class AppModule {}
