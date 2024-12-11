9d994ad24cd71fdfb7556790c8847f26161247c0
Maxomys <mistrz7losos@live.com>
2024-12-11 04:41:53 +0100

    update documentation

--- src/star-wars/analysis/analysis.model.ts
+++ src/star-wars/analysis/analysis.model.ts
@@ -3,9 +3,12 @@ import { UniqueWord } from './unique-word.model';
 
 @ObjectType({ description: 'Analysis of the opening_crawl.' })
 export class Analysis {
-  @Field(() => [UniqueWord])
+  @Field(() => [UniqueWord], { description: 'An array of unique words that are in all opening crawls' })
   unique_words: UniqueWord[];
 
-  @Field(() => [String])
+  @Field(() => [String], {
+    description:
+      'A name of a character from the API that appears the most often across all of the openings of the films.',
+  })
   most_mentioned_character: string[];
 }
