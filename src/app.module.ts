c69f9d09bc2186f20c615f937f218f854d7fe5f6
Maxomys <mistrz7losos@live.com>
2024-12-11 02:57:36 +0100

    remove rest controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -36,7 +35,6 @@ import { AnalysisModule } from './star-wars/analysis/analysis.module';
     SwapiModule,
     AnalysisModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
